57cbf4668e02505e33550eaa7c4d960d4637dc5e
Duyên Ng <huongduyen.work@gmail.com>
2023-10-29 09:46:50 +0700

    Feat: DishModel and DishViewModel

---
+++ Areas/Admin/ViewModels/DishViewModel.cs
@@ -0,0 +1,28 @@
+﻿using ASP.NET_Core_Website_QuanLyTiecCuoiLanHue.Models;
+using System.ComponentModel;
+
+namespace ASP.NET_Core_Website_QuanLyTiecCuoiLanHue.Areas.Admin.ViewModels
+{
+    public class DishViewModel
+    {
+        [DisplayName("Tên món ăn")]
+        public string? DishName { get; set; }
+        [DisplayName("Giá")]
+        public decimal Price { get; set; }
+
+        public int DishTypeId { get; set; }
+        [DisplayName("Đơn vị tính")]
+        public int UnitId { get; set; }
+
+        public Dish ToDish(QlDichVuNauTiecLanHueContext context)
+        {
+            return new Dish()
+            {
+                DishName = DishName,
+                Price = Price,
+                DishTypeId = DishTypeId,
+                UnitId = UnitId,
+            };
+        }
+    }
+}
--- Models/Unit.cs
+++ Models/Unit.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace ASP.NET_Core_Website_QuanLyTiecCuoiLanHue.Models;
 
 public partial class Unit
 {
     public int UnitId { get; set; }
-
+    [DisplayName("Tên đơn vị")]
     public string UnitName { get; set; } = null!;
-
+    [DisplayName("Mô tả")]
     public string? Description { get; set; }
 
     public virtual ICollection<Dish> Dishes { get; set; } = new List<Dish>();
