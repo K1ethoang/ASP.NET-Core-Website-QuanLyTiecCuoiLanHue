9ff803fdcc0f56067da706e843cd37e55c38c215
Kiet Hoang <kiethoang101@gmail.com>
2023-10-30 10:30:19 +0700

    Add view: Edit, Detail

--- Areas/Admin/ViewModels/PartyViewModel.cs
+++ Areas/Admin/ViewModels/PartyViewModel.cs
@@ -10,7 +10,6 @@ namespace ASP.NET_Core_Website_QuanLyTiecCuoiLanHue.Areas.Admin.ViewModels
     [Keyless]
     public class PartyViewModel
     {
-
         // PartyName
         [Required(ErrorMessage = "{0} không được trống")]
         [StringLength(maximumLength: 255, MinimumLength = 0, ErrorMessage = "Độ dài tối đa 255 ký tự")]
@@ -20,19 +19,28 @@ namespace ASP.NET_Core_Website_QuanLyTiecCuoiLanHue.Areas.Admin.ViewModels
         [Required(ErrorMessage = "{0} không được trống")]
         [Range(0, 100, ErrorMessage = "{0} trong khoảng [{1} và {2}]")]
         [DisplayName("Số bàn")]
-        public int? Quantity { get; set; }
+        public int? Quantity
+        {
+            get; set;
+        }
         // Date
         [Required(ErrorMessage = "{0} không được trống")]
         [DataType(DataType.Date, ErrorMessage = "{0} Không hợp lệ")]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         [CheckDateRange(ErrorMessage = "Phải lớn hơn ngày hôm nay")]
         [DisplayName("Ngày")]
-        public DateTime? Date { get; set; }
+        public DateTime? Date
+        {
+            get; set;
+        }
         // Time
         [Required(ErrorMessage = "{0} không được trống")]
         [DataType(DataType.Time, ErrorMessage = "{0} Không hợp lệ")]
         [DisplayName("Giờ")]
-        public TimeSpan? Time { get; set; }
+        public TimeSpan? Time
+        {
+            get; set;
+        }
         [DisplayName("Ghi chú")]
         public string? Note
         {
@@ -48,10 +56,16 @@ namespace ASP.NET_Core_Website_QuanLyTiecCuoiLanHue.Areas.Admin.ViewModels
         //CustomerId
         [Required(ErrorMessage = "{0} không được trống")]
         [DisplayName("Khách hàng")]
-        public int CustomerId { get; set; }
+        public int CustomerId
+        {
+            get; set;
+        }
         //PartyTypeId
         [DisplayName("Loại tiệc")]
-        public int PartyTypeId { get; set; }
+        public int PartyTypeId
+        {
+            get; set;
+        }
         public class CheckDateRangeAttribute : ValidationAttribute
         {
             protected override ValidationResult IsValid(object value, ValidationContext validationContext)
@@ -69,22 +83,21 @@ namespace ASP.NET_Core_Website_QuanLyTiecCuoiLanHue.Areas.Admin.ViewModels
 
         public Party ToParty(QlDichVuNauTiecLanHueContext context)
         {
-            return new Party() {
-            PartyName = PartyName,
-            Status = Party.COMING_SOON,
-            HasMenu = false,
-            Date = Date,
-            Time = Time,
-            Quantity = Quantity,
-            Location = Location,
-            Note = Note,
-            PartyTypeId = PartyTypeId,
-            CustomerId = CustomerId,
-            // Foreign Objects
-            PartyType = context.PartyTypes.Find(PartyTypeId) ?? new PartyType(),
-            Customer = context.Customers.Find(CustomerId) ?? new Customer(),
-
-
+            return new Party()
+            {
+                PartyName = PartyName,
+                Status = Party.COMING_SOON,
+                HasMenu = false,
+                Date = Date,
+                Time = Time,
+                Quantity = Quantity,
+                Location = Location,
+                Note = Note,
+                PartyTypeId = PartyTypeId,
+                CustomerId = CustomerId,
+                // Foreign Objects
+                PartyType = context.PartyTypes.Find(PartyTypeId) ?? new PartyType(),
+                Customer = context.Customers.Find(CustomerId) ?? new Customer(),
             };
         }
     }
