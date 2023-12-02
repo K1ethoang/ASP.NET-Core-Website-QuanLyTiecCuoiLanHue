981e1545b21b6314c3e17b9206a7c9cab376b31e
Duyên Ng <huongduyen.work@gmail.com>
2023-12-02 23:28:08 +0700

    getting names

--- Areas/Admin/Controllers/PartiesController.cs
+++ Areas/Admin/Controllers/PartiesController.cs
@@ -18,271 +18,301 @@ using static ASP.NET_Core_Website_QuanLyTiecCuoiLanHue.Areas.Admin.ViewModels.Me
 
 namespace ASP.NET_Core_Website_QuanLyTiecCuoiLanHue.Areas.Admin.Controllers
 {
-    [Area("Admin")]
-    public class PartiesController : Controller
-    {
-        private readonly QlDichVuNauTiecLanHueContext _context;
+	[Area("Admin")]
+	public class PartiesController : Controller
+	{
+		private readonly QlDichVuNauTiecLanHueContext _context;
 
-        public PartiesController(QlDichVuNauTiecLanHueContext context)
-        {
-            _context = context;
-        }
+		public PartiesController(QlDichVuNauTiecLanHueContext context)
+		{
+			_context = context;
+		}
 
-        // GET: Admin/Parties
-        [Route("admin/parties")]
-        public async Task<IActionResult> Index()
-        {
-            var qlDichVuNauTiecLanHueContext = _context.Parties
-                .Include(p => p.Customer)
-                .Include(p => p.PartyType);
-            return View(await qlDichVuNauTiecLanHueContext.ToListAsync());
-        }
+		// GET: Admin/Parties
+		[Route("admin/parties")]
+		public async Task<IActionResult> Index()
+		{
+			var qlDichVuNauTiecLanHueContext = _context.Parties
+				.Include(p => p.Customer)
+				.Include(p => p.PartyType);
+			return View(await qlDichVuNauTiecLanHueContext.ToListAsync());
+		}
 
-        // GET: Admin/Parties/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Parties == null)
-            {
-                return NotFound();
-            }
+		// GET: Admin/Parties/Details/5
+		public async Task<IActionResult> Details(int? id)
+		{
+			if (id == null || _context.Parties == null)
+			{
+				return NotFound();
+			}
 
-            var party = await _context.Parties
-                .Include(p => p.Customer)
-                .Include(p => p.PartyType)
-                .FirstOrDefaultAsync(m => m.PartyId == id);
+			var party = await _context.Parties
+				.Include(p => p.Customer)
+				.Include(p => p.PartyType)
+				.FirstOrDefaultAsync(m => m.PartyId == id);
 
-            if (party == null)
-            {
-                return NotFound();
-            }
+			if (party == null)
+			{
+				return NotFound();
+			}
 
-            return View(party);
-        }
+			return View(party);
+		}
 
-        // GET: Admin/Parties/Create
-        public IActionResult Create()
-        {
-            PartyViewModel vm = new PartyViewModel();
+		// GET: Admin/Parties/Create
+		public IActionResult Create()
+		{
+			PartyViewModel vm = new PartyViewModel();
 
-            var customerSelectList = _context.Customers
-                .ToList()
-                .Select(c => new SelectListItem(
-                    text: c.PhoneNumber + " - " + c.CusName,
-                    value: Convert.ToString(c.CustomerId)))
-                .ToList() ?? new List<SelectListItem>();
+			var customerSelectList = _context.Customers
+				.ToList()
+				.Select(c => new SelectListItem(
+					text: c.PhoneNumber + " - " + c.CusName,
+					value: Convert.ToString(c.CustomerId)))
+				.ToList() ?? new List<SelectListItem>();
 
-            ViewData["CustomerList"] = customerSelectList;
+			ViewData["CustomerList"] = customerSelectList;
 
-            ViewData["PartyTypeId"] = new SelectList(_context.PartyTypes, "PartyTypeId", "Name");
+			ViewData["PartyTypeId"] = new SelectList(_context.PartyTypes, "PartyTypeId", "Name");
 
-            return View(vm);
-        }
+			return View(vm);
+		}
 
-        // POST: Admin/Parties/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(
-                                        [Bind("PartyId,PartyName,Quantity,Date,Time,Location,Note,PartyTypeId,CustomerId")]
-                                        PartyViewModel vm)
-        {
-            if (ModelState.IsValid)
-            {
-                Party party = vm.ToParty(context: _context);
-                _context.Add(party);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
+		// POST: Admin/Parties/Create
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create(
+										[Bind("PartyId,PartyName,Quantity,Date,Time,Location,Note,PartyTypeId,CustomerId")]
+										PartyViewModel vm)
+		{
+			if (ModelState.IsValid)
+			{
+				Party party = vm.ToParty(context: _context);
+				_context.Add(party);
+				await _context.SaveChangesAsync();
+				return RedirectToAction(nameof(Index));
+			}
 
-            var customerSelectList = _context.Customers
-                .ToList()
-                .Select(c => new SelectListItem(
-                    text: c.PhoneNumber + " - " + c.CusName,
-                    value: Convert.ToString(c.CustomerId)))
-                .ToList() ?? new List<SelectListItem>();
+			var customerSelectList = _context.Customers
+				.ToList()
+				.Select(c => new SelectListItem(
+					text: c.PhoneNumber + " - " + c.CusName,
+					value: Convert.ToString(c.CustomerId)))
+				.ToList() ?? new List<SelectListItem>();
 
-            ViewData["CustomerList"] = customerSelectList;
+			ViewData["CustomerList"] = customerSelectList;
 
-            ViewData["PartyTypeId"] = new SelectList(_context.PartyTypes, "PartyTypeId", "Name", vm.PartyTypeId);
+			ViewData["PartyTypeId"] = new SelectList(_context.PartyTypes, "PartyTypeId", "Name", vm.PartyTypeId);
 
-            return View(vm);
-        }
+			return View(vm);
+		}
 
-        // GET: Admin/Parties/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		// GET: Admin/Parties/Edit/5
+		public async Task<IActionResult> Edit(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var party = await _context.Parties.FindAsync(id);
+			var party = await _context.Parties.FindAsync(id);
 
-            if (party == null)
-            {
-                return NotFound();
-            }
+			if (party == null)
+			{
+				return NotFound();
+			}
 
-            var customerSelectList = _context.Customers
-                .ToList()
-                .Select(c => new SelectListItem(
-                    text: c.PhoneNumber + " - " + c.CusName,
-                    value: Convert.ToString(c.CustomerId)))
-                .ToList() ?? new List<SelectListItem>();
+			var customerSelectList = _context.Customers
+				.ToList()
+				.Select(c => new SelectListItem(
+					text: c.PhoneNumber + " - " + c.CusName,
+					value: Convert.ToString(c.CustomerId)))
+				.ToList() ?? new List<SelectListItem>();
 
-            PartyViewModel vm = new PartyViewModel(party);
+			PartyViewModel vm = new PartyViewModel(party);
 
-            ViewData["CustomerList"] = customerSelectList;
+			ViewData["CustomerList"] = customerSelectList;
 
-            ViewData["PartyTypeId"] = new SelectList(_context.PartyTypes, "PartyTypeId", "Name", party.PartyTypeId);
+			ViewData["PartyTypeId"] = new SelectList(_context.PartyTypes, "PartyTypeId", "Name", party.PartyTypeId);
 
-            return View(vm);
-        }
+			return View(vm);
+		}
 
-        // POST: Admin/Parties/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id,
-                    [Bind("PartyName,Quantity,Date,Time,Location,Note,Status,HasMenu,PartyTypeId,CustomerId")]
-                                            PartyViewModel vm)
-        {
-            var party = await _context.Parties.Where(p => p.PartyId == id).FirstAsync();
+		// POST: Admin/Parties/Edit/5
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(int id,
+					[Bind("PartyName,Quantity,Date,Time,Location,Note,Status,HasMenu,PartyTypeId,CustomerId")]
+											PartyViewModel vm)
+		{
+			var party = await _context.Parties.Where(p => p.PartyId == id).FirstAsync();
 
-            Console.ForegroundColor = ConsoleColor.Red;
-            await Console.Out.WriteLineAsync(party.ToJson());
-            Console.ResetColor();
+			Console.ForegroundColor = ConsoleColor.Red;
+			await Console.Out.WriteLineAsync(party.ToJson());
+			Console.ResetColor();
 
-            if (party == null || party.PartyId != id)
-            {
-                return NotFound();
-            }
+			if (party == null || party.PartyId != id)
+			{
+				return NotFound();
+			}
 
-            _context.Attach(party);
+			_context.Attach(party);
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    // You should use the Entry() method in case you want to update all the fields in your object.
-                    // Also keep in mind you cannot change the field id(key) therefore first set the Id to the same as you edit.
-                    // https://stackoverflow.com/a/47340877/20423795
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					// You should use the Entry() method in case you want to update all the fields in your object.
+					// Also keep in mind you cannot change the field id(key) therefore first set the Id to the same as you edit.
+					// https://stackoverflow.com/a/47340877/20423795
 
-                    _context.Entry(party).State = EntityState.Modified;
-                    var newParty = vm.ToParty(context: _context);
-                    newParty.PartyId = party.PartyId;
-                    _context.Entry(party).CurrentValues.SetValues(newParty);
-                    //_context.Update(party);
+					_context.Entry(party).State = EntityState.Modified;
+					var newParty = vm.ToParty(context: _context);
+					newParty.PartyId = party.PartyId;
+					_context.Entry(party).CurrentValues.SetValues(newParty);
+					//_context.Update(party);
 
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!PartyExists(party.PartyId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!PartyExists(party.PartyId))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction(nameof(Index));
+			}
 
-            var customerSelectList = _context.Customers
-                .ToList()
-                .Select(c => new SelectListItem(
-                    text: c.PhoneNumber + " - " + c.CusName,
-                    value: Convert.ToString(c.CustomerId)))
-                .ToList<SelectListItem>() ?? new List<SelectListItem>();
+			var customerSelectList = _context.Customers
+				.ToList()
+				.Select(c => new SelectListItem(
+					text: c.PhoneNumber + " - " + c.CusName,
+					value: Convert.ToString(c.CustomerId)))
+				.ToList<SelectListItem>() ?? new List<SelectListItem>();
 
-            ViewData["CustomerList"] = customerSelectList;
+			ViewData["CustomerList"] = customerSelectList;
 
-            ViewData["PartyTypeId"] = new SelectList(_context.PartyTypes, "PartyTypeId", "Name", party.PartyTypeId);
-            return View(vm);
-        }
+			ViewData["PartyTypeId"] = new SelectList(_context.PartyTypes, "PartyTypeId", "Name", party.PartyTypeId);
+			return View(vm);
+		}
 
-        // GET: Admin/Parties/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Parties == null)
-            {
-                return NotFound();
-            }
+		// GET: Admin/Parties/Delete/5
+		public async Task<IActionResult> Delete(int? id)
+		{
+			if (id == null || _context.Parties == null)
+			{
+				return NotFound();
+			}
 
-            var party = await _context.Parties
-                .Include(p => p.Customer)
-                .Include(p => p.PartyType)
-                .FirstOrDefaultAsync(m => m.PartyId == id);
-            if (party == null)
-            {
-                return NotFound();
-            }
+			var party = await _context.Parties
+				.Include(p => p.Customer)
+				.Include(p => p.PartyType)
+				.FirstOrDefaultAsync(m => m.PartyId == id);
+			if (party == null)
+			{
+				return NotFound();
+			}
 
-            return View(party);
-        }
+			return View(party);
+		}
 
-        // POST: Admin/Parties/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.Parties == null)
-            {
-                return Problem("Entity set 'QlDichVuNauTiecLanHueContext.Parties'  is null.");
-            }
-            var party = await _context.Parties.FindAsync(id);
-            if (party != null)
-            {
-                _context.Parties.Remove(party);
-            }
+		// POST: Admin/Parties/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			if (_context.Parties == null)
+			{
+				return Problem("Entity set 'QlDichVuNauTiecLanHueContext.Parties'  is null.");
+			}
+			var party = await _context.Parties.FindAsync(id);
+			if (party != null)
+			{
+				_context.Parties.Remove(party);
+			}
 
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+			await _context.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
+		}
 
-        private bool PartyExists(int id)
-        {
-            return (_context.Parties?.Any(e => e.PartyId == id)).GetValueOrDefault();
-        }
-        // GET
-        public async Task<IActionResult> CreateMenu(int? id)
-        {
-            if (id == null || !PartyExists(id!.Value))
-            {
-                return NotFound();
-            }
+		private bool PartyExists(int id)
+		{
+			return (_context.Parties?.Any(e => e.PartyId == id)).GetValueOrDefault();
+		}
+		// GET
+		[HttpGet]
+		public async Task<IActionResult> CreateMenu(int id)
+		{
+			if (id == null || !PartyExists(id))
+			{
+				return NotFound();
+			}
+			var party = _context.Parties.Where(p => p.PartyId == id).FirstOrDefault();
+			ViewData["DateAndTime"] = party!.Date.ToString();
 
-            Menu newMenu = new Menu(id!.Value, context: _context);
+			CreateMenuViewModel model = new CreateMenuViewModel();
 
-            if (newMenu == null)
-            {
-                return Empty;
-            }
 
-            //ViewData["DishTypeList"] = new SelectList(_context.DishTypes, "DishTypeId", "TypeName");
+			//ViewData["DishTypeList"] = new SelectList(_context.DishTypes, "DishTypeId", "TypeName");
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine(id);
+			Console.ResetColor();
+			//return View(menu);
 
-            return View(newMenu);
-        }
-        // POST
-        [HttpPost]
-        public async Task<IActionResult> CreateMenu(Menu menu)
-        {
+			List<MenuItem> items = await _context.Dishes
+				.Select(d => new MenuItem()
+				{
+					DishId = d.DishId,
+					DishName = d.DishName!,
+					UnitName = d.Unit.UnitName,
+					DishType = d.DishType.TypeName,
+					Qty = 0,
+					Selected = false,
+					Price = (int)d.Price,
+				})
+				.ToListAsync();
 
-            foreach (MenuItem item in menu.AvailableItems)
-            {
-                if (item.Qty > 0)
-                {
-                    menu.Items.Add(item);
-                }
-            }
+			ViewBag.MinQty = party!.Quantity;
+			ViewBag.Id = id;
 
-            //ViewData["DishTypeList"] = new SelectList(_context.DishTypes, "DishTypeId", "TypeName");
+			model.Id = id;
+			model.Items = items;
 
-            return View(menu);
-        }
-    }
+			return View(model);
+		}
+		// POST
+		[HttpPost]
+		public async Task<IActionResult> CreateMenu( CreateMenuViewModel model)
+		{
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("SEND {0}",model.Id);
+			if (ModelState.IsValid)
+			{
+
+				Console.WriteLine("VALID");
+				foreach (var item in model.Items)
+				{
+					if (item.Selected )
+					{
+						Console.WriteLine(item.DishId.ToString()+"-"+item.DishName+"-"+item.Qty.ToString());
+					}
+				}
+				RedirectToAction("Details",  new {id = model.Id});
+			}
+			Console.ResetColor();
+
+			//ViewData["DishTypeList"] = new SelectList(_context.DishTypes, "DishTypeId", "TypeName");
+
+			return RedirectToAction("Index");
+		}
+	}
 }
--- Areas/Admin/ViewModels/Menu.cs
+++ Areas/Admin/ViewModels/Menu.cs
@@ -1,101 +1,116 @@
 ﻿using ASP.NET_Core_Website_QuanLyTiecCuoiLanHue.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.ComponentModel;
 using static ASP.NET_Core_Website_QuanLyTiecCuoiLanHue.Areas.Admin.ViewModels.Menu;
 
 namespace ASP.NET_Core_Website_QuanLyTiecCuoiLanHue.Areas.Admin.ViewModels
 {
-    public class Menu
-    {
-        public int PartyId { get; }
-        public List<MenuItem> Items { get; set; } = new List<MenuItem>();
-        public List<MenuItem> AvailableItems { get; set; }
-        public int MinQty;
+	[BindProperties]
+	public class Menu
+	{
+		//[BindProperty(Name = "id")]
+		public int PartyId { get; set; }
+		public List<MenuItem> Items { get; set; } = new List<MenuItem>();
+		public List<MenuItem> AvailableItems { get; set; }
+		public int MinQty;
 
-        // CONSTRUCTORS
-        public Menu() {
-           
-        }
-        private void initAvailableItems(QlDichVuNauTiecLanHueContext context)
-        {
-            
+		// CONSTRUCTORS
+		public Menu()
+		{
 
-            AvailableItems = context.Dishes
-                //.Include(d => d.Unit.UnitName)
-                //.Include(d => d.DishType.TypeName)
-                .Select(d => new MenuItem()
-                {
-                    DishId = d.DishId,
-                    DishName = d.DishName!,
-                    UnitName = d.Unit.UnitName,
-                    DishType = d.DishType.TypeName,
-                    Qty = 0
-                })
-                .ToList();
-        }
-        private void  initMinQty (QlDichVuNauTiecLanHueContext context)
-        {
-            MinQty = context.Parties.Where(p=>p.PartyId==PartyId).First().Quantity ?? 0;
-        }
-        public Menu(int PartyId, QlDichVuNauTiecLanHueContext context) 
-        {
-            this.PartyId = PartyId;
-            initMinQty(context);
-            initAvailableItems(context);
-        }
-        public Menu(Party party, QlDichVuNauTiecLanHueContext context)
-        {
-            PartyId = party.PartyId;
-            initMinQty(context);
-            initAvailableItems(context);
+		}
+		public static List<MenuItem> GetAvailableItems(QlDichVuNauTiecLanHueContext context)
+		{
 
-            if (party.HasMenu) { }
-        }
 
-        bool DishExists(int id)
-        {
-            return Items.Any(i => i.DishId == id);
-        }
-        public class MenuItem
-        {
-            //
-            // Type nesting
-            // https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/nested-types
-            //
-            Menu? FromMenu;
-            int _DishId;
-            public int DishId { get => _DishId; set => _DishId = value; }
+			return context.Dishes
+				//.Include(d => d.Unit.UnitName)
+				//.Include(d => d.DishType.TypeName)
+				.Select(d => new MenuItem()
+				{
+					DishId = d.DishId,
+					DishName = d.DishName!,
+					UnitName = d.Unit.UnitName,
+					DishType = d.DishType.TypeName,
+					Qty = 0
+				})
+				.ToList();
+		}
 
-            //[DisplayName("Tên món ăn")]
-            public string DishName;
-            public string UnitName;
-            public string DishType;
-            [DisplayName("Số lượng")]
-            public int Qty { get; set; }
+		private void initMinQty(QlDichVuNauTiecLanHueContext context)
+		{
+			MinQty = context.Parties.Where(p => p.PartyId == PartyId).First().Quantity ?? 0;
+		}
+		public Menu(int PartyId, QlDichVuNauTiecLanHueContext context)
+		{
+			this.PartyId = PartyId;
+			MinQty = context.Parties.Where(p => p.PartyId == PartyId).First().Quantity ?? 0;
+			AvailableItems = GetAvailableItems(context);
+		}
+		public Menu(Party party, QlDichVuNauTiecLanHueContext context)
+		{
+			PartyId = party.PartyId;
+			MinQty = context.Parties.Where(p => p.PartyId == PartyId).First().Quantity ?? 0;
+			AvailableItems = GetAvailableItems(context);
 
-            public MenuItem() { }
-            public MenuItem(DishViewModel vm) { }
-            public MenuItem(Dish dish, int qty = 1)
-            {
-                _DishId = dish.DishId;
-                Qty = qty;
-            }
-            public MenuItem(int dishId, int qty = 1)
-            {
-                _DishId = dishId;
-                Qty = qty;
-            }
-            public static MenuItem ToMenuItem(Dish dish, string UnitName)
-            {
-                return new MenuItem()
-                {
-                    DishId = dish.DishId,
-                    DishName = dish.DishName,
-                    UnitName = UnitName,
-                    Qty = 0
-                };
-            }
+			if (party.HasMenu) { }
+		}
 
-        }
-    }
+		bool DishExists(int id)
+		{
+			return Items.Any(i => i.DishId == id);
+		}
+
+	}
+	[Keyless]
+	public class MenuItem
+	{
+		//
+		// Type nesting
+		// https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/nested-types
+		//
+		//Menu? FromMenu;
+		public int DishId { get; set; }
+		public int Price {  get; set; }
+
+
+		//[DisplayName("Tên món ăn")]
+		public string? DishName {  get; set; }
+		public string? UnitName {  get; set; }
+		public string? DishType {  get; set; }
+		public bool Selected {  get; set; }
+		[DisplayName("Số lượng")]
+		public int Qty { get; set; }
+
+		//public MenuItem() { }
+		//public MenuItem(DishViewModel vm) { }
+		//public MenuItem(Dish dish, int qty = 1)
+		//{
+		//	_DishId = dish.DishId;
+		//	Qty = qty;
+		//}
+		//public MenuItem(int dishId, int qty = 1)
+		//{
+		//	_DishId = dishId;
+		//	Qty = qty;
+		//}
+		public static MenuItem ToMenuItem(Dish dish, string UnitName)
+		{
+			return new MenuItem()
+			{
+				DishId = dish.DishId,
+				DishName = dish.DishName,
+				UnitName = UnitName,
+				Qty = 0
+			};
+		}
+
+	}
+	public class CreateMenuViewModel
+	{
+	[BindProperty]
+		public int Id { get; set; } 
+		public List<MenuItem> Items { get; set; } = new List<MenuItem>();
+	}
 }
